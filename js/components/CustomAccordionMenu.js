a5246d96ff05c9f955e2a4b563af0a9cc98fce4d
Nox <the0neWhoKnocks@users.noreply.github.com>
2021-08-20 14:27:53 -0700

    feat: more accurate open/close for Accordion

--- js/components/CustomAccordionMenu.js
+++ js/components/CustomAccordionMenu.js
@@ -28,6 +28,7 @@
 })();
 
 (() => {
+  const CSS_VAR__ANIM_DURATION = '--anim-duration';
   const CSS_VAR__COLOR__INNER_BORDER = '--color--inner-border';
   const CSS_VAR__COLOR__OUTER_BORDER = '--color--outer-border';
   const CSS_VAR__COLOR__HOVER = '--color--hover';
@@ -35,6 +36,7 @@
   const CSS_VAR__WIDTH__OUTER_BORDER = '--width--outer-border';
   const ROOT_CLASS = 'accordion-menu-group';
   
+  const DEFAULT__ANIM_DURATION = '150ms';
   const DEFAULT__COLOR__INNER_BORDER = '#eee';
   const DEFAULT__COLOR__OUTER_BORDER = '#ddd';
   const DEFAULT__COLOR__HOVER = '#eee';
@@ -42,14 +44,77 @@
   const DEFAULT__WIDTH__OUTER_BORDER = 2;
   
   class CustomAccordionMenuGroup extends HTMLElement {
+    get open() {
+      return this.hasAttribute('open');
+    }
+    set open(value) {
+      const open = value === '' || value === 'true' || value === true;
+      const changed = open !== this.open;
+      
+      if (changed) {
+        if (open) {
+          this.setAttribute('open', '');
+          
+          // animation should only happen when mounted
+          if (this.mounted) {
+            this.els.items.classList.add('animating');
+            
+            requestAnimationFrame(() => {
+              this.els.items.addEventListener('transitionend', () => {
+                this.els.items.classList.remove('animating');
+                this.els.items.style.height = '';
+              }, { once: true });
+              
+              this.els.items.style.height = `${this.els.itemsWrapper.offsetHeight}px`;
+            });
+          }
+        }
+        else {
+          if (this.mounted) {
+            this.els.items.style.height = `${this.els.itemsWrapper.offsetHeight}px`;
+            
+            requestAnimationFrame(() => {
+              this.removeAttribute('open');
+              
+              if (this.mounted) {
+                this.els.items.addEventListener('transitionend', () => {
+                  this.els.items.classList.remove('animating');
+                }, { once: true });
+            
+                this.els.items.style.height = '';
+                this.els.items.classList.add('animating');
+              }
+            });
+          }
+          else {
+            this.removeAttribute('open');
+          }
+        }
+      }
+    }
+    
+    static get observedAttributes() {
+      return ['open'];
+    }
+    
+    attributeChangedCallback(attr, oldVal, newVal) {
+      const empty = oldVal === '' && (newVal === null || newVal === undefined);
+      
+      if (!empty && oldVal !== newVal) {
+        let _newVal = newVal;
+        
+        switch (attr) {
+          default: { this[attr] = _newVal; }
+        }
+      }
+    }
+    
     constructor() {
       super();
       
       this.attachShadow({ mode: 'open' });
       
       const { shadowRoot } = this;
-      
-      // TODO - use animations for open/close so that visibility can be set
       
       shadowRoot.innerHTML = `
         <style>
@@ -58,6 +123,7 @@
           }
           
           :host {
+            ${CSS_VAR__ANIM_DURATION}: ${DEFAULT__ANIM_DURATION};
             ${CSS_VAR__COLOR__INNER_BORDER}: ${DEFAULT__COLOR__INNER_BORDER};
             ${CSS_VAR__COLOR__OUTER_BORDER}: ${DEFAULT__COLOR__OUTER_BORDER};
             ${CSS_VAR__COLOR__HOVER}: ${DEFAULT__COLOR__HOVER};
@@ -78,6 +144,31 @@
             border-bottom: none;
           }
           
+          .${ROOT_CLASS}__btn {
+            width: 100%;
+            font-size: 1em;
+            padding: 0;
+            border: none;
+            border-bottom: solid var(${CSS_VAR__WIDTH__OUTER_BORDER}) var(${CSS_VAR__COLOR__OUTER_BORDER});
+            background: transparent;
+          }
+            
+          .${ROOT_CLASS}__btn-label {
+            width: 100%;
+            text-align: left;
+            text-transform: uppercase;
+            background: transparent;
+            position: relative;
+            display: block;
+            box-sizing: border-box;
+            user-select: none;
+          }
+          ::slotted([slot="label"]) {
+            padding: 1em;
+            display: block;
+            cursor: pointer;
+          }
+          
           .${ROOT_CLASS}__plus-minus {
             width: var(${CSS_VAR__INDICATOR_DIAMETER});
             height: var(${CSS_VAR__INDICATOR_DIAMETER});
@@ -95,7 +186,7 @@
             display: block;
             position: absolute;
             top: 0;
-            transition: all 0.25s;
+            transition: all var(${CSS_VAR__ANIM_DURATION});
           }
           .${ROOT_CLASS}__minus1::after,
           .${ROOT_CLASS}__minus2::after {
@@ -112,6 +203,32 @@
           }
           .${ROOT_CLASS}__minus2 {
             transform: rotate(90deg);
+          }
+          :host([open]) .${ROOT_CLASS}__minus1,
+          :host([open]) .${ROOT_CLASS}__minus2 {
+            transform: rotate(180deg);
+          }
+          
+          .${ROOT_CLASS}__items {
+            height: 0px;
+            overflow: hidden;
+            transition: height var(${CSS_VAR__ANIM_DURATION});
+          }
+          :host([open]) .${ROOT_CLASS}__items:not(.animating) {
+            height: auto;
+          }
+          
+          .${ROOT_CLASS}__items-wrapper {
+            visibility: hidden;
+            transform: translateY(-100%);
+            transition: transform var(${CSS_VAR__ANIM_DURATION});
+          }
+          :host([open]) .${ROOT_CLASS}__items-wrapper,
+          .animating .${ROOT_CLASS}__items-wrapper {
+            visibility: visible;
+          }
+          :host([open]) .${ROOT_CLASS}__items-wrapper {
+            transform: translateY(0%);
           }
           
           ::slotted([slot="item"]) {
@@ -130,59 +247,10 @@
           ::slotted([slot="item"]:hover) {
             background: var(${CSS_VAR__COLOR__HOVER});
           }
-          
-          .${ROOT_CLASS}__items {
-            overflow: hidden;
-            max-height: 0;
-            transition: max-height 0.25s cubic-bezier(0, 1, 0, 1) -0.1s;
-            visibility: hidden;
-          }
-          
-          .${ROOT_CLASS}__btn {
-            border-bottom: solid var(${CSS_VAR__WIDTH__OUTER_BORDER}) var(${CSS_VAR__COLOR__OUTER_BORDER});
-          }
-            
-          .${ROOT_CLASS}__btn-label {
-            width: 100%;
-            text-align: left;
-            text-transform: uppercase;
-            background: transparent;
-            position: relative;
-            display: block;
-            box-sizing: border-box;
-            user-select: none;
-          }
-          ::slotted([slot="label"]) {
-            padding: 1em;
-            display: block;
-            cursor: pointer;
-          }
-            
-          .${ROOT_CLASS}__checkbox {
-            opacity: 0;
-            position: absolute;
-            top: 0;
-            left: 0;
-          }
-          .${ROOT_CLASS}__checkbox:checked ~ .${ROOT_CLASS}__btn .${ROOT_CLASS}__minus1,
-          .${ROOT_CLASS}__checkbox:checked ~ .${ROOT_CLASS}__btn .${ROOT_CLASS}__minus2 {
-            transform: rotate(180deg);
-          }
-          .${ROOT_CLASS}__checkbox:checked ~ .${ROOT_CLASS}__items {
-            max-height: 9999px;
-            transition: max-height 0.25s cubic-bezier(1, 0, 1, 0) 0s;
-            visibility: visible;
-          }
-          
-          .${ROOT_CLASS}__checkbox:focus ~ .${ROOT_CLASS}__btn .${ROOT_CLASS}__btn-label {
-            outline: solid 2px blue;
-          }
         </style>
         
-        
-        <li class="${ROOT_CLASS}">
-          <input id="itemsGroup" type="checkbox" class="${ROOT_CLASS}__checkbox">
-          <div class="${ROOT_CLASS}__btn">
+        <div class="${ROOT_CLASS}">
+          <button class="${ROOT_CLASS}__btn" type="button">
             <label class="${ROOT_CLASS}__btn-label" for="itemsGroup">
               <slot name="label"></slot>
               <div class="${ROOT_CLASS}__plus-minus">
@@ -190,12 +258,31 @@
                 <span class="${ROOT_CLASS}__minus2"></span>
               </div>
             </label>
+          </button>
+          <div class="${ROOT_CLASS}__items">
+            <div class="${ROOT_CLASS}__items-wrapper">
+              <slot name="item"></slot>
+            </div>
           </div>
-          <ul class="${ROOT_CLASS}__items">
-            <slot name="item"></slot>
-          </ul>
-        </li>
+        </div>
       `;
+      
+      this.els = {
+        groupBtn: shadowRoot.querySelector(`.${ROOT_CLASS}__btn`),
+        items: shadowRoot.querySelector(`.${ROOT_CLASS}__items`),
+        itemsWrapper: shadowRoot.querySelector(`.${ROOT_CLASS}__items-wrapper`),
+      };
+      
+      this.toggleGroup = this.toggleGroup.bind(this);
+    }
+    
+    connectedCallback() {
+      this.mounted = true;
+      this.els.groupBtn.addEventListener('click', this.toggleGroup);
+    }
+    
+    toggleGroup() {
+      this.open = !this.open;
     }
   }
 
