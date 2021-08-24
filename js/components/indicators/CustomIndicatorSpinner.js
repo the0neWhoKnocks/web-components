2fcd659f0ca4479acbdef914799cf3f4fe51de14
Trevor Lemon <duhlemman.dev@gmail.com>
2021-08-24 01:43:02 -0700

    feat: IndicatorSpinner svg

--- js/components/indicators/CustomIndicatorSpinner.js
+++ js/components/indicators/CustomIndicatorSpinner.js
@@ -3,6 +3,7 @@
   const DEFAULT__SPEED = 500;
   const ROOT_CLASS = 'spinner';
   const TYPE__CSS = 'css';
+  const TYPE__SVG = 'svg';
   
   class CustomIndicatorSpinner extends HTMLElement {
     get color() {
@@ -24,7 +25,7 @@
       return this.getAttribute('type') || TYPE__CSS;
     }
     set type(value) {
-      ([TYPE__CSS].includes(value))
+      ([TYPE__CSS, TYPE__SVG].includes(value))
         ? this.setAttribute('type', value)
         : this.setAttribute('type', TYPE__CSS);
     }
@@ -56,6 +57,10 @@
     
     connectedCallback() {
       switch (this.type) {
+        case TYPE__SVG:
+          this.renderSVGSpinner();
+          break;
+        
         case TYPE__CSS:
         default:
           this.renderCSSSpinner();
@@ -101,6 +106,68 @@
         </style>
         
         <div class="${ROOT_CLASS}"></div>
+      `;
+    }
+    
+    renderSVGSpinner() {
+      const DIAMETER = 20;
+      const RADIUS = DIAMETER / 2;
+      const STROKE_WIDTH = 4;
+      const OFFSET = STROKE_WIDTH / 2;
+      
+      const points = `0,${RADIUS} ${RADIUS},0`;
+      const curve = points.split(' ').map((coords, ndx, arr) => {
+        const [x, y] = coords.split(',');
+        let _x = x;
+        let _y = y;
+        
+        if (_x < RADIUS) _x += OFFSET;
+        else if (_x > RADIUS) _x -= OFFSET;
+        
+        if (_y < RADIUS) _y += OFFSET;
+        else if (_y > RADIUS) _y -= OFFSET;
+        
+        let _points = `M${_x},${_y}`;
+        
+        if (ndx > 0) {
+          const [prevX, prevY] = arr[ndx - 1].split(',');
+          _points = `C${prevX + OFFSET},${Math.max(prevY, _y)/2} ${Math.max(prevX, _x)/2},${_y} ${_x},${_y}`;
+        }
+        
+        return _points;
+      }).join(' ');
+      
+      this.shadowRoot.innerHTML = `
+        <style>
+          @keyframes spin {
+            0% { transform: rotate(0deg); }
+            100% { transform: rotate(360deg); }
+          }
+          
+          svg {
+            width: 1em;
+            height: 1em;
+          }
+          
+          circle {
+            opacity: 0.15;
+            fill: none;
+            stroke: ${this.color};
+            stroke-width: ${STROKE_WIDTH};
+          }
+          
+          path {
+            stroke: ${this.color};
+            stroke-width: ${STROKE_WIDTH};
+            transform-origin: center;
+            animation: spin ${this.speed}ms linear infinite;
+          }
+        </style>
+        
+        <svg viewBox="0 0 ${DIAMETER} ${DIAMETER}" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="${RADIUS}" cy="${RADIUS}" r="${RADIUS - OFFSET}" />
+          <path stroke-linecap="round" fill="none" d="${curve}"></path>
+        </svg>
       `;
     }
   }
