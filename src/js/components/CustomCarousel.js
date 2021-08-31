f5a90574e95220780e590d7de106f0736e99b00a
Nox <the0neWhoKnocks@users.noreply.github.com>
2021-08-31 08:24:46 -0700

    feat: Carousel uses slot for items

--- src/js/components/CustomCarousel.js
+++ src/js/components/CustomCarousel.js
@@ -1,43 +1,27 @@
 (() => {
-  const ASPECT_RATIO__LANDSCAPE = 'landscape';
-  const ASPECT_RATIO__PORTRAIT = 'portrait';
-  const ASPECT_RATIO__SQUARE = 'square';
-  const CSS_VAR__COLOR__BTN__BG = '--color--btn--bg'
-  const CSS_VAR__COLOR__BTN__FG = '--color--btn--fg'
-  const CSS_VAR__ITEM_DIAMETER = '--item-diameter';
+  const CSS_VAR__CAROUSEL_HEIGHT = '--carousel-height';
+  const CSS_VAR__COLOR__BTN__BG = '--color--btn--bg';
+  const CSS_VAR__COLOR__BTN__FG = '--color--btn--fg';
   const EVENT__ADVANCED = 'carouselAdvanced';
   const EVENT__REGRESSED = 'carouselRegressed';
   const ROOT_CLASS = 'carousel';
   
+  const DEFAULT__CAROUSEL_HEIGHT = '15em';
   const DEFAULT__COLOR__BTN__BG = '#333';
   const DEFAULT__COLOR__BTN__FG = '#eee';
-  const DEFAULT__ITEM_DIAMETER = '8em';
   
   class CustomCarousel extends HTMLElement {
-    set customStyles(styles) {
-      this.els.customStyles.textContent = styles;
+    get height() {
+      return this.getAttribute('height') || DEFAULT__CAROUSEL_HEIGHT;
     }
-    
-    get itemDiameter() {
-      return this.getAttribute('itemDiameter') || DEFAULT__ITEM_DIAMETER;
-    }
-    set itemDiameter(value) {
-      this.setAttribute('itemDiameter', value);
-      this.style.setProperty(CSS_VAR__ITEM_DIAMETER, value);
-    }
-    
-    static get aspectRatios() {
-      return {
-        landscape: ASPECT_RATIO__LANDSCAPE,
-        portrait: ASPECT_RATIO__PORTRAIT,
-        square: ASPECT_RATIO__SQUARE,
-      };
+    set height(value) {
+      this.setAttribute('height', value);
+      this.style.setProperty(CSS_VAR__CAROUSEL_HEIGHT, value);
     }
     
     static get observedAttributes() {
       return [
-        'customstyles',
-        'itemdiameter',
+        'height',
       ];
     }
     
@@ -55,8 +39,6 @@
         let _newVal = newVal;
         
         switch (attr) {
-          case 'customstyles': { this.customStyles = _newVal; break; }
-          case 'itemdiameter': { this.itemDiameter = _newVal; break; }
           default: { this[attr] = _newVal; }
         }
       }
@@ -78,16 +60,23 @@
           :host {
             ${CSS_VAR__COLOR__BTN__BG}: ${DEFAULT__COLOR__BTN__BG};
             ${CSS_VAR__COLOR__BTN__FG}: ${DEFAULT__COLOR__BTN__FG};
-            ${CSS_VAR__ITEM_DIAMETER}: ${DEFAULT__ITEM_DIAMETER};
+            ${CSS_VAR__CAROUSEL_HEIGHT}: ${DEFAULT__CAROUSEL_HEIGHT};
+            --section-nav-spacing: 0.5em;
+            --section-nav-item-diameter: 1.25em;
             
             font-family: Helvetica, Arial, sans-serif;
+            display: block;
           }
           
-          slot {
-            display: none;
+          .${ROOT_CLASS}-wrapper {
+            height: calc(var(${CSS_VAR__CAROUSEL_HEIGHT}) + (var(--section-nav-item-diameter) + var(--section-nav-spacing)));
+          }
+          .${ROOT_CLASS}-wrapper.no-nav {
+            height: var(${CSS_VAR__CAROUSEL_HEIGHT});
           }
           
           .${ROOT_CLASS} {
+            height: var(${CSS_VAR__CAROUSEL_HEIGHT});
             display: flex;
           }
           
@@ -98,54 +87,17 @@
           }
           
           .${ROOT_CLASS}__items {
-            display: flex;
             transition: transform 0.4s;
-            transform: translateX(0%);
+            transform: translateX(0px);
             touch-action: none;
           }
-          .${ROOT_CLASS}__items.is--dragging {
+          .${ROOT_CLASS}__items.disable-transition {
             transition: none;
-          }
-          
-          .${ROOT_CLASS}__item {
-            flex-shrink: 0;
-            background-color: #eee;
-            background-size: cover;
-          }
-          .${ROOT_CLASS}__item > * {
-            display: block;
-          }
-          .${ROOT_CLASS}__item > img {
-            pointer-events: none;
-          }
-          .${ROOT_CLASS}__item[ar="${ASPECT_RATIO__SQUARE}"] {
-            width: var(${CSS_VAR__ITEM_DIAMETER});
-          }
-          .${ROOT_CLASS}__item[ar="${ASPECT_RATIO__SQUARE}"] > * {
-            width: 100%;
-          }
-          .${ROOT_CLASS}__item[ar="${ASPECT_RATIO__LANDSCAPE}"],
-          .${ROOT_CLASS}__item[ar="${ASPECT_RATIO__PORTRAIT}"] {
-            height: var(${CSS_VAR__ITEM_DIAMETER});
-          }
-          .${ROOT_CLASS}__item[ar="${ASPECT_RATIO__LANDSCAPE}"] > *,
-          .${ROOT_CLASS}__item[ar="${ASPECT_RATIO__PORTRAIT}"] > * {
-            height: 100%;
-          }
-          .${ROOT_CLASS}__item:not(:first-of-type) {
-            margin-left: 3px;
-          }
-          .${ROOT_CLASS}__item > :not(a) {
-            pointer-events: none;
-          }
-          .${ROOT_CLASS}__item > a,
-          .${ROOT_CLASS}__item > img {
-            user-select: none;
           }
           
           .${ROOT_CLASS}__ui-btn {
             color: var(${CSS_VAR__COLOR__BTN__FG});
-            width: 5%;
+            width: 3em;
             padding: 0;
             border: none;
             background: var(${CSS_VAR__COLOR__BTN__BG});
@@ -163,11 +115,11 @@
           
           .${ROOT_CLASS}-sections-nav {
             text-align: center;
-            margin-top: 0.5em;
+            margin-top: var(--section-nav-spacing);
           }
           .${ROOT_CLASS}-sections-nav button {
-            width: 1.25em;
-            height: 1.25em;
+            width: var(--section-nav-item-diameter);
+            height: var(--section-nav-item-diameter);
             border: solid 1px var(${CSS_VAR__COLOR__BTN__BG});
             border-radius: 100%;
             background: var(${CSS_VAR__COLOR__BTN__FG});
@@ -183,6 +135,7 @@
             cursor: pointer;
           }
           
+          
           .${ROOT_CLASS}-wrapper.no-nav .${ROOT_CLASS}__ui-btn,
           .${ROOT_CLASS}-wrapper.no-nav .${ROOT_CLASS}-sections-nav {
             display: none;
@@ -190,12 +143,13 @@
         </style>
         <style id="customStyles"></style>
         
-        <slot name="item"></slot>
         <div class="${ROOT_CLASS}-wrapper no-nav">
           <div class="${ROOT_CLASS}">
             <button type="button" class="${ROOT_CLASS}__ui-btn is--prev">&lt;</button>
             <div class="${ROOT_CLASS}__items-container">
-              <div class="${ROOT_CLASS}__items"></div>
+              <div class="${ROOT_CLASS}__items">
+                <slot name="item"></slot>
+              </div>
             </div>
             <button type="button" class="${ROOT_CLASS}__ui-btn is--next">&gt;</button>
           </div>
@@ -207,13 +161,12 @@
         carousel: shadowRoot.querySelector(`.${ROOT_CLASS}`),
         customStyles: shadowRoot.getElementById('customStyles'),
         items: shadowRoot.querySelector(`.${ROOT_CLASS}__items`),
+        itemSlot: shadowRoot.querySelector('slot[name="item"]'),
         nextBtn: shadowRoot.querySelector(`.${ROOT_CLASS}__ui-btn.is--next`),
         prevBtn: shadowRoot.querySelector(`.${ROOT_CLASS}__ui-btn.is--prev`),
         sectionsNav: shadowRoot.querySelector(`.${ROOT_CLASS}-sections-nav`),
         wrapper: shadowRoot.querySelector(`.${ROOT_CLASS}-wrapper`),
       };
-      this.atStart = true;
-      this.viewAreaOffset = 0;
     }
     
     connectedCallback() {
@@ -225,8 +178,9 @@
       this.handleSectionIndicatorClick = this.handleSectionIndicatorClick.bind(this);
       this.handleSlotChange = this.handleSlotChange.bind(this);
       this.renderNav = this.renderNav.bind(this);
+      this.reset = this.reset.bind(this);
       
-      window.addEventListener('resize', this.renderNav);
+      window.addEventListener('resize', this.reset);
       this.shadowRoot.addEventListener('slotchange', this.handleSlotChange);
       this.els.nextBtn.addEventListener('click', this.handleNextClick);
       this.els.prevBtn.addEventListener('click', this.handlePrevClick);
@@ -237,73 +191,22 @@
     }
     
     disconnectedCallback() {
-      this.els.nextBtn.removeEventListener('click', this.handleNextClick);
-      this.els.prevBtn.removeEventListener('click', this.handlePrevClick);
-      this.els.items.removeEventListener('pointerdown', this.handlePointerDown);
+      window.removeEventListener('resize', this.reset);
     }
     
-    setViewOffset(direction, multiplier = 1) {
-      const viewArea = this.els.items.offsetWidth;
-      const sectionOffset = viewArea * multiplier;
-      const carouselXPos = this.els.items.getBoundingClientRect().x;
-      let newOffset;
+    handleNextClick() {
+      let nextNdx = this.sectionNdx + 1;
       
-      this.atEnd = false;
-      this.atStart = false;
-      
-      // an item can be half in view so find the offset of the cropped item
-      const items = [...this.shadowRoot.querySelectorAll(`.${ROOT_CLASS}__item`)];
-      for (let i=0; i<items.length; i++) {
-        const item = items[i];
-        const { width, x } = item.getBoundingClientRect();
-        const _x = x - carouselXPos;
+      if (nextNdx <= this.sectionsCount) {
+        this.prevSectionNdx = this.sectionNdx;
+        this.sectionNdx = nextNdx;
         
-        if (direction === 'next') {
-          if ((_x + width) >= (this.viewAreaOffset + sectionOffset)) {
-            const overlap = this.viewAreaOffset
-              ? (this.viewAreaOffset + viewArea) - _x
-              : viewArea - _x;
-            
-            newOffset = (this.viewAreaOffset + sectionOffset) - overlap;
-            
-            if ((this.els.items.scrollWidth - newOffset) < sectionOffset) {
-              newOffset = this.els.items.scrollWidth - viewArea;
-              this.atEnd = true;
-            }
-            
-            break;
-          }
-        }
-        else {
-          const prevOffset = this.viewAreaOffset - sectionOffset;
-          
-          if (prevOffset <= 0) {
-            newOffset = 0;
-            this.atStart = true;
-            break;
-          }
-          else if (_x >= prevOffset) {
-            newOffset = _x;
-            if (newOffset < 0) {
-              newOffset = 0;
-              this.atStart = true;
-            }
-            break;
-          }
-        }
+        this.update();
       }
-      
-      if (newOffset !== undefined) this.viewAreaOffset = newOffset;
-    }
-    
-    handleNextClick({ multiplier } = {}) {
-      this.setViewOffset('next', multiplier);
-      this.scrollItems('next');
-      this.renderNav();
     }
     
     handlePointerDown(ev) {
-      this.els.items.classList.add('is--dragging');
+      this.els.items.classList.add('disable-transition');
       const currPos = this.els.items.style.transform.match(/([-.\d]+)px/) || [0, 0];
       this.startX = ev.x;
       this.startPos = +currPos[1];
@@ -320,77 +223,107 @@
     handlePointerUp() {
       window.removeEventListener('pointermove', this.handlePointerMove);
       window.removeEventListener('pointerup', this.handlePointerUp);
-      this.els.items.classList.remove('is--dragging');
+      this.els.items.classList.remove('disable-transition');
       
-      // based on drag direction, scroll the to next or prev
-      if (this.endX < this.startX) this.handleNextClick();
-      else this.handlePrevClick();
+      const [, currXPos] = this.els.items.style.transform.match(/translateX\(([\d-.]+)px\)/) || [0, 0];
+      const xPos = +currXPos;
+      
+      // based on drag direction, scroll to next or prev
+      if (this.endX < this.startX) {
+        const maxOffset = -(this.els.items.scrollWidth - this.els.items.offsetWidth);
+        if (xPos < maxOffset) this.sectionNdx = this.sectionOffsets.length - 2;
+        
+        this.handleNextClick();
+      }
+      else {
+        if (xPos > 0) this.sectionNdx = 1;
+        
+        this.handlePrevClick();
+      }
     }
     
-    handlePrevClick({ multiplier } = {}) {
-      this.setViewOffset('prev', multiplier);
-      this.scrollItems('prev');
-      this.renderNav();
+    handlePrevClick() {
+      let prevNdx = this.sectionNdx - 1;
+      
+      if (prevNdx >= 0) {
+        this.prevSectionNdx = this.sectionNdx;
+        this.sectionNdx = prevNdx;
+        
+        this.update();
+      }
     }
     
     handleSectionIndicatorClick({ target }) {
       if (target.nodeName === 'BUTTON') {
-        const currNdx = +[...this.els.sectionsNav.childNodes].find(el => el.disabled).dataset.ndx;
-        const newNdx = +target.dataset.ndx;
-        const multiplier = Math.abs(currNdx - newNdx);
+        this.prevSectionNdx = this.sectionNdx;
+        this.sectionNdx = +target.dataset.ndx;
         
-        if (newNdx > currNdx) this.handleNextClick({ multiplier });
-        else this.handlePrevClick({ multiplier });
+        this.update();
       }
     }
     
     handleSlotChange({ target: slot }) {
       switch (slot.name) {
-        case 'item': {
-          this.els.items.innerHTML = slot.assignedNodes().map(el => {
-            const clone = el.cloneNode(true);
-            const aspectRatio = clone.getAttribute('ar');
-            clone.removeAttribute('ar');
-            clone.removeAttribute('slot');
-            
-            return `
-              <div class="${ROOT_CLASS}__item" ar="${aspectRatio}">
-                ${clone.outerHTML}
-              </div>
-            `;
-          }).join('');
-          
-          [...this.els.items.querySelectorAll('a, img')].forEach(el => {
-            el.draggable = false;
-          });
-          
-          this.renderNav();
-          
-          break;
-        }
+        case 'item': this.reset(); break;
       }
     }
     
     renderNav() {
       clearTimeout(this.navDebounce);
       this.navDebounce = setTimeout(() => {
-        const parentWidth = this.parentNode.offsetWidth;
-        const itemsWidth = this.els.items.scrollWidth;
+        const itemsViewWidth = this.els.items.offsetWidth;
+        const itemsFullWidth = this.els.items.scrollWidth;
+        this.sectionsCount = 1;
+        this.sectionOffsets = [0];
         
-        if (itemsWidth > parentWidth) {
+        // NOTE: Since adding or removing the nav can cause a shift in dimensions,
+        // don't proceed after the update, just wait, and then try to render the
+        // nav again, but with the proper dimensions.
+        if (
+          itemsFullWidth > itemsViewWidth
+          && this.els.wrapper.classList.contains('no-nav')
+        ) {
           this.els.wrapper.classList.remove('no-nav');
+          requestAnimationFrame(this.renderNav);
+          return;
         }
-        else if (!this.els.wrapper.classList.contains('no-nav')) {
+        else if (
+          itemsFullWidth <= itemsViewWidth
+          && !this.els.wrapper.classList.contains('no-nav')
+        ) {
           this.els.wrapper.classList.add('no-nav');
+          requestAnimationFrame(this.renderNav);
+          return;
         }
         
         if (!this.els.wrapper.classList.contains('no-nav')) {
-          this.els.prevBtn.disabled = this.atStart;
-          this.els.nextBtn.disabled = this.atEnd;
+          let x = 0;
+          let sectionWidth = 0;
+          this.els.itemSlot.assignedNodes().forEach((item, ndx, items) => {
+            const { marginLeft } = getComputedStyle(item);
+            const width = item.offsetWidth + parseFloat(marginLeft);
+            
+            sectionWidth += width;
+            
+            if (sectionWidth >= itemsViewWidth) {
+              this.sectionsCount += 1;
+              this.sectionOffsets.push(x);
+              sectionWidth = width;
+            }
+            
+            // there's empty space in the last section so trim the last offset
+            if (
+              ndx === items.length - 1
+              && sectionWidth < itemsViewWidth
+            ) {
+              const lastNdx = this.sectionOffsets.length - 1;
+              const emptySpace = itemsViewWidth - sectionWidth;
+              this.sectionOffsets[lastNdx] = this.sectionOffsets[lastNdx] - emptySpace;
+            }
+            
+            x += width;
+          });
           
-          const itemsWidth = this.els.items.scrollWidth;
-          const viewWidth = this.els.items.offsetWidth;
-          this.sectionsCount = Math.ceil(itemsWidth/viewWidth);
           // only render indicators when number of sections have changed
           if (
             !this.els.sectionsNav.childNodes.length
@@ -401,25 +334,51 @@
             }).join('');
           }
           
-          const sectionNdx = Math.ceil(this.viewAreaOffset / viewWidth);
+          this.els.prevBtn.disabled = this.sectionNdx === 0;
+          this.els.nextBtn.disabled = this.sectionNdx === (this.sectionOffsets.length - 1);
           this.els.sectionsNav.childNodes.forEach((indicator, ndx) => {
-            indicator.disabled = ndx === sectionNdx;
+            indicator.disabled = ndx === this.sectionNdx;
           });
         }
       }, 100);
     }
     
-    scrollItems(direction) {
-      const newTransform = `translateX(${-this.viewAreaOffset}px)`;
+    reset() {
+      this.els.items.classList.add('disable-transition');
+      
+      window.requestAnimationFrame(() => {
+        this.prevSectionNdx = 0;
+        this.sectionNdx = 0;
+        this.sectionsCount = 1;
+        this.viewAreaOffset = 0;
+        this.els.items.style.transform = 'translateX(0px)';
+        
+        window.requestAnimationFrame(() => {
+          this.els.items.classList.remove('disable-transition');
+          
+          this.renderNav();
+        });
+      });
+    }
+    
+    scrollItems() {
+      const newTransform = `translateX(${-this.sectionOffsets[this.sectionNdx]}px)`;
       
       if (newTransform !== this.els.items.style.transform) {
         this.els.items.addEventListener('transitionend', () => {
-          const ev = direction === 'next' ? EVENT__ADVANCED : EVENT__REGRESSED;
+          const ev = (this.sectionNdx > this.prevSectionNdx)
+            ? EVENT__ADVANCED
+            : EVENT__REGRESSED;
           this.dispatchEvent(new CustomEvent(ev, { bubbles: true }));
         }, { once: true });
         
         this.els.items.style.transform = newTransform;
       }
+    }
+    
+    update() {
+      this.scrollItems();
+      this.renderNav();
     }
   }
 
